3262606d99a247895de3f7734390423824b42063
Vignesh Karthikeyan <vkarthi@purdue.edu>
2017-11-22 13:48:22 -0600

    Changed Menu to match text, To do Next: Setup card view

--- app/src/main/java/com/example/vignesh/buttonpress/MainActivity.java
+++ app/src/main/java/com/example/vignesh/buttonpress/MainActivity.java
@@ -103,17 +103,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_inventory) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_addItem) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_notifications) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_logout) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.rate_us) {
 
         }
 
