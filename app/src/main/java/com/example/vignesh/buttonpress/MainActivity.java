ba0f17062e272da10dd496f18b1bb9b9cd3b7f44
Vignesh Karthikeyan <vkarthi@purdue.edu>
2017-11-28 19:55:27 -0500

    now setup so that strings are entered, To do values are to be changed back to

--- app/src/main/java/com/example/vignesh/buttonpress/MainActivity.java
+++ app/src/main/java/com/example/vignesh/buttonpress/MainActivity.java
@@ -12,9 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,11 +33,350 @@ public class MainActivity extends AppCompatActivity
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference("ButtonPressedTimes");
+        final DatabaseReference spice1 = database.getReference("Spice1");
+        final DatabaseReference spice2 = database.getReference("Spice2");
+        final DatabaseReference spice3 = database.getReference("Spice3");
+        final DatabaseReference spice4 = database.getReference("Spice4");
+        final DatabaseReference spice5 = database.getReference("Spice5");
+        final DatabaseReference spice6 = database.getReference("Spice6");
+        final DatabaseReference spice7 = database.getReference("Spice7");
+        final DatabaseReference spice8 = database.getReference("Spice8");
+        final DatabaseReference spice9 = database.getReference("Spice9");
+        final DatabaseReference spice10 = database.getReference("Spice10");
+        final DatabaseReference spice11 = database.getReference("Spice11");
+        final DatabaseReference spice12 = database.getReference("Spice12");
+        final DatabaseReference spice13 = database.getReference("Spice13");
+        final DatabaseReference spice14 = database.getReference("Spice14");
+        final DatabaseReference spice15 = database.getReference("Spice15");
+        final DatabaseReference spice16 = database.getReference("Spice16");
+        final DatabaseReference spice17 = database.getReference("Spice17");
+        final DatabaseReference spice18 = database.getReference("Spice18");
 
-        myRef.setValue(0);
 
 
+
+        //textview stuff
+        final TextView tv1 = (TextView)findViewById(R.id.spice1);
+
+
+        spice1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv1.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv2 = (TextView)findViewById(R.id.spice2);
+
+
+        spice2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv2.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv3 = (TextView)findViewById(R.id.spice3);
+
+
+        spice3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv3.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv4 = (TextView)findViewById(R.id.spice4);
+
+
+        spice4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv4.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv5 = (TextView)findViewById(R.id.spice5);
+
+
+        spice5.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv5.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv6 = (TextView)findViewById(R.id.spice6);
+
+
+        spice6.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv6.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv7 = (TextView)findViewById(R.id.spice7);
+
+
+        spice7.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv7.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv8 = (TextView)findViewById(R.id.spice8);
+
+
+        spice8.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv8.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv9 = (TextView)findViewById(R.id.spice9);
+
+
+        spice9.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv9.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv10 = (TextView)findViewById(R.id.spice10);
+
+
+        spice10.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv10.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv11 = (TextView)findViewById(R.id.spice11);
+
+
+        spice11.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv11.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv12 = (TextView)findViewById(R.id.spice12);
+
+
+        spice12.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv12.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv13 = (TextView)findViewById(R.id.spice13);
+
+
+        spice13.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv13.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //textview stuff
+        final TextView tv14 = (TextView)findViewById(R.id.spice14);
+
+
+        spice14.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv14.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv15 = (TextView)findViewById(R.id.spice15);
+
+
+        spice15.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv15.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv16 = (TextView)findViewById(R.id.spice16);
+
+
+        spice16.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv16.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv17 = (TextView)findViewById(R.id.spice17);
+
+
+        spice17.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv17.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //textview stuff
+        final TextView tv18 = (TextView)findViewById(R.id.spice18);
+
+
+        spice18.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                tv18.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         //FAB
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         final boolean[] flag = {true};
@@ -43,13 +386,13 @@ public class MainActivity extends AppCompatActivity
                 if(flag[0]) {
                     Snackbar.make(view, "Updated Firebase", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
-                    myRef.setValue(1);
+                  //  spice1.setValue(1);
                     flag[0] = false;
                 }
                 else if(!flag[0]){
                     Snackbar.make(view, "Updated Firebase", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
-                    myRef.setValue(0);
+                   // spice1.setValue(0);
                     flag[0] = true;
                 }
             }
